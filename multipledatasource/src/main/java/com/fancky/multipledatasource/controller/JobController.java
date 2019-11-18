4ec4b452c144acdcc46ecdebe9175f05229c6363
fancky <709737588@qq.com>
2019-11-18 10:42:02 +0800

    target
    1

--- multipledatasource/src/main/java/com/fancky/multipledatasource/controller/JobController.java
+++ multipledatasource/src/main/java/com/fancky/multipledatasource/controller/JobController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/job")
 public class JobController {
+
     @Autowired
 //    @Resource
     private JobService jobService;
@@ -20,6 +21,7 @@ public class JobController {
     {
         return "hello world";
     }
+
     @PostMapping("/insertJob")
     public MessageResult<Void> insertJob(@RequestBody Job job) {
         return jobService.insert(job);
